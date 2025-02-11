8f1db23a1b3e37c43ed87580608cf7afc7fb95cf
MiraiOka <uaofmsbct.9g@gmail.com>
2025-02-12 08:58:15 +0900

    buttonの作成

---
+++ Assets/250211_ColorBall/Scripts/Button/ColorBall_ButtonController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBall_ButtonController : MonoBehaviour
+{
+    public void OnRedLeftButtonClicked()
+    {
+    }
+
+    public void OnRedRightButtonClicked()
+    {
+    }
+
+    public void OnRedDownButtonClicked()
+    {
+    }
+
+    public void OnRedUpButtonClicked()
+    {
+    }
+
+    public void OnBlueLeftButtonClicked()
+    {
+    }
+
+    public void OnBlueRightButtonClicked()
+    {
+    }
+
+    public void OnBlueDownButtonClicked()
+    {
+    }
+
+    public void OnBlueUpButtonClicked()
+    {
+    }
+}
